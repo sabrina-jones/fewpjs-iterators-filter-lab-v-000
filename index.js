f320e6e436a59e2494cf0f427e3a6e8bb0b6e823
Sabrina Jones <sabrina.jones87@gmail.com>
2021-03-15 03:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(function(d) {
+    return d.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function(d) {
+    return d.substring(0,1) === name.substring(0,1)
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(d) {
+    return d.name === name
+  })
+}
